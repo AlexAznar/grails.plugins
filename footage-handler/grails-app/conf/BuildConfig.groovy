386fba7ab7c01bc06925394978fba5e5f0649ecf
Christian Rommel <Christian.reg@web.de>
2013-01-26 18:50:31 +0100

    footage-handler resource plugin

--- footage-handler/grails-app/conf/BuildConfig.groovy
+++ footage-handler/grails-app/conf/BuildConfig.groovy
@@ -13,15 +13,8 @@ grails.project.dependency.resolution = {
         grailsPlugins()
         grailsHome()
         grailsCentral()
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenCentral()
+        
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
@@ -31,5 +24,6 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ':aws:1.1.9.2'
+        runtime ":resources:1.2-RC1"
     }
 }
